c333401449655e847dd803b01b1a53963b89ae63
Emine Shaban <emineshaban@icloud.com>
2023-01-28 15:42:14 +0200

    done task2

--- task-2/register.js
+++ task-2/register.js
@@ -1,57 +1,85 @@
 
-document.getElementById('registerButton').addEventListener('click', function e() {
+document.getElementById('registerButton').addEventListener('click', function e(event) {
+    event.preventDefault()
     let firstName = document.getElementById('firstName')
     let lastName = document.getElementById('lastName')
     let email = document.getElementById('email')
     let password = document.getElementById('password')
     let confirmPassword = document.getElementById('confirmPassword')
-console.log(firstName.value)
 
-let obj={}
+    let obj = {}
 
-let nameRGEX = /[a-zA-Z]/g
-let firstNameResult = nameRGEX.test(firstName.value);
-let lastNameResult = nameRGEX.test(lastName.value);
-let passwordRGEX = /([a-zA-Z]{1,})+(\d{1,})\w+/g
-let passwordResult = passwordRGEX.test(password.value);
+    let nameRGEX = /[a-zA-Z]/g
+    let firstNameResult = nameRGEX.test(firstName.value);
+    let lastNameResult = nameRGEX.test(lastName.value);
+    let passwordRGEX = /^(?=.*[0-9])(?=.*[a-zA-Z])(?=\S+$).{6,25}$/gm
+    let passwordResult = passwordRGEX.test(password.value);
+    let emailRGEX = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g
+    let emailResult = emailRGEX.test(email.value);
 
 
-    if(firstName.value.length <= 2 || firstName.value.length >= 20 ){
+    if (firstName.value.length <= 2 || firstName.value.length >= 20) {
         firstName.classList.add("redBorder")
         alert('First name must be at least 2 characters and max 20 characters!')
-    }else if(firstNameResult == false){
+    } else if (firstNameResult == false) {
         firstName.classList.add("redBorder")
         alert('First name must contain only latin characters!')
-    }else{
+    } else {
         obj.firstName = firstName.value
         firstName.classList.remove('redBorder')
     }
 
 
-    if(lastName.value.length <= 2 || lastName.value.length >= 25){
+    if (lastName.value.length <= 2 || lastName.value.length >= 20) {
         lastName.classList.add("redBorder")
-        alert('Last name must be at least 2 characters and max 20 characters and must contain only latin characters!')
-    }else if(lastNameResult == false){
+        alert('Last name must be at least 2 characters and max 20 characters')
+    } else if (lastNameResult == false) {
         lastName.classList.add("redBorder")
         alert('Last name must contain only latin characters!')
-    }else{
+    } else {
         obj.lastName = lastName.value
         lastName.classList.remove('redBorder')
     }
 
+    if (emailResult == false) {
 
+        email.classList.add("redBorder")
+        alert('Email is invalid!')
+    } else {
+        obj.email = email.value
+        email.classList.remove('redBorder')
+    }
 
-    // }else if(password.value.length <= 6 || password.value.length >= 25 || passwordResult == false){
-    //     console.log(passwordResult)
-    //     console.log(password.value)
+    if (password.value.length <= 6 || password.value.length >= 25) {
+        password.classList.add("redBorder")
+        alert('Password must be at least 6 characters and max 25 characters!')
+    } else if (passwordResult == false) {
 
-    //     password.classList.add("redBorder")
-    //     alert('Password must be at least 6 characters and max 25 characters and must contain only latin characters and numbers!')
-    // }else if(password.value !=confirmPassword.value){
-    //     password.classList.add("redBorder")
-    //     confirmPassword.classList.add("redBorder")
-    //     alert('Password do not match!')
-    // }
+        password.classList.add("redBorder")
+        alert('Password must contain at least 1 latin character and 1 number!')
+    } else {
+        obj.password = password.value
+        password.classList.remove('redBorder')
+    }
 
-console.log( obj)
+    if (password.value != confirmPassword.value) {
+        password.classList.add("redBorder")
+        confirmPassword.classList.add("redBorder")
+        alert('Passwords do not match!')
+    } else {
+        obj.confirmPassword = confirmPassword.value
+        password.classList.remove('redBorder')
+        confirmPassword.classList.remove('redBorder')
+    }
+
+    if (Object.keys(obj).length == 5) {
+        console.log(obj)
+        
+        document.getElementById('register-input').style.display = 'none'
+        document.getElementById('registerDiv').style.display = 'block'
+        document.getElementById('registerDivP').style.display = 'block'
+    
+
+    }
+
 })
