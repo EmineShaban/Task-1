77cfb6bc6b6ffcc74f61ae42f897bf501da40470
Emine Shaban <emineshaban@icloud.com>
2023-01-29 19:09:55 +0200

    create react app

---
+++ task-3/meteor-js/src/App.js
@@ -0,0 +1,40 @@
+// import logo from './logo.svg';
+import './App.css';
+// import Template from 'react-templates';
+
+
+
+function App() {
+//   const data = [
+//     {
+//         name: 'Mia',
+//         age: '10'
+//     },
+//     {
+//         name: 'Ivan',
+//         age: '35'
+//     },
+//     {
+//         name: 'Elena',
+//         age: '25'
+//     },
+// ];
+// Template.aaaa.helpers
+let aaa = 'dldldlldldld'
+const handleClick = () =>{
+console.log('click')
+}
+  return (
+
+
+    <div className="App">
+    <p>fflfkfklfkfk</p>
+
+    <p>{aaa}</p>
+
+<button onClick={handleClick}>Click</button>
+    </div>
+  );
+}
+
+export default App;
